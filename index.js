ee2f9c8c97052048e5c81faa914b4108d81f0267
prime8489 <botsend0@gmail.com>
2025-04-07 16:32:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors"); // <== Import CORS
 const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(cors()); // <== Enable CORS for all routes
 
 // Serve media files
 app.use("/media", express.static(path.join(__dirname, "public")));
